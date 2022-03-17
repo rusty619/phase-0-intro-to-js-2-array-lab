87bca27c3d50c74ba771f382b157d4414a6c0bd4
rusty619 <rusty6916@gmail.com>
2022-03-17 09:58:53 -0400

    commit tree a169b2b7d9ef3a13a2fa2e9de7151583838cb6b8

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+
+let destructivelyAppendCat = (name) => {
+    cats.push(name)
+}
+
+let destructivelyPrependCat = (name) => {
+    cats.unshift(name)
+}
+
+let destructivelyRemoveLastCat = () => {
+    cats.pop()
+}
+
+let destructivelyRemoveFirstCat = () => {
+    cats.shift()
+}
+
+let appendCat = (name) => {
+    var newCats = [...cats,name]
+    return newCats
+} 
+
+let prependCat = (name) => {
+    var newCats = [...cats]
+    newCats.unshift(name)
+    return newCats
+}
+
+let removeLastCat = () => {
+    var newCats = [...cats]
+    newCats.pop()
+    return newCats
+}
+
+let removeFirstCat = () => {
+    var newCats = [...cats]
+    newCats.shift()
+    return newCats
+}
